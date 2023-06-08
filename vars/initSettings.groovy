985bcaca0cb5f5542a200c3538512c2c10f883a0
Amit <amityash83@gmail.com>
2023-06-08 18:44:03 +0530

    updated variable

--- vars/initSettings.groovy
+++ vars/initSettings.groovy
@@ -2,7 +2,8 @@ import settings.GetImageName
 import settings.UpdateSettings
 
 def call() {
-	def jobName = env.JOB_NAME
+	// def jobName = env.JOB_NAME
+    def jobName = to-image-rhel8-gen
 	def matchedImgName = GetImageName.parseImageName(jobName)
 	
 	if (matchedImgName) {
